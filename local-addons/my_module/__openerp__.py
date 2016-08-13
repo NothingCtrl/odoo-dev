3bf83b72e75ed4bfbf5c77a2a45b98494a5cb0e6
NothingCtrl <nothingctrl@gmail.com>
2016-08-14 00:35:43 +0700

    End Chapter 3 of Odoo Development Cookbook

---
+++ local-addons/my_module/__openerp__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "My Module",
+    'summary': "Short subtitle phrase",
+    'description': """Long description""",
+    'author': "Duong Bao Thang",
+    'license': "AGPL-3",
+    'website': "http://camratus.com",
+    'category': "Uncategorized",
+    'version': "9.0.1.0.0",
+    'depends': ['base'],
+    'data': ['views/library_book.xml'],
+    #'demo': ['demo.xml'],
+}
